edca59a515389b20e2c56c3c121875fa073d0660
n-ogawa <fy59.kou-729@live.jp>
2026-01-01 10:56:43 +0900

    Throw error when file is not DER encoded ASN.1

--- ZipishLens/Signature/Parser.cs
+++ ZipishLens/Signature/Parser.cs
@@ -7,6 +7,15 @@ public class Parser
     public static SignedData Parse(ReadOnlyMemory<byte> signature)
     {
         var reader = new AsnReader(signature, AsnEncodingRules.DER);
+        try
+        {
+            var sequenceReader = reader.ReadSequence();
+        }
+        catch (AsnContentException e)
+        {
+            throw new InvalidDataException("Invalid signature format", e);
+        }
+
 
         return new SignedData();
     }
