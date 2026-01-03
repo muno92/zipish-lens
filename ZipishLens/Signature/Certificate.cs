c748db9862cf4dcd1a81040c4e4f80ade76113c0
n-ogawa <fy59.kou-729@live.jp>
2026-01-03 14:35:04 +0900

    Fixed intermediate certificate detection.

--- ZipishLens/Signature/Certificate.cs
+++ ZipishLens/Signature/Certificate.cs
@@ -2,4 +2,8 @@ namespace ZipishLens.Signature;
 
 public record Certificate(
     CertInfo CertInfo
-);
+)
+{
+    public bool IsIntermediate(IReadOnlyList<Certificate> chain) =>
+        chain.Any(c => c.CertInfo.Issuer.CommonName == CertInfo.Subject.CommonName);
+}
