e6695b1788f3ce8435ade3d6e59e7636b5b34590
n-ogawa <fy59.kou-729@live.jp>
2026-01-03 18:05:57 +0900

    Refactor certificate view and streamline intermediate certificate handling.

--- ZipishLens/Signature/Resource/Certificate.cs
+++ ZipishLens/Signature/Resource/Certificate.cs
@@ -2,8 +2,4 @@ namespace ZipishLens.Signature.Resource;
 
 public record Certificate(
     CertInfo CertInfo
-)
-{
-    public bool IsIntermediate(IReadOnlyList<Certificate> chain) =>
-        chain.Any(c => c.CertInfo.Issuer.CommonName == CertInfo.Subject.CommonName);
-}
+);
--- ZipishLens/Signature/Resource/SignedData.cs
+++ ZipishLens/Signature/Resource/SignedData.cs
@@ -7,4 +7,32 @@ public record SignedData(
     string DigestAlgorithmIdentifiers,
     IReadOnlyList<Certificate> Certificates,
     IReadOnlyList<SignerInfo> SignerInfos
-);
+)
+{
+    /// <summary>
+    /// Returns the certificate that signed the signature.
+    /// </summary>
+    public Certificate IssuerCertificate
+    {
+        get
+        {
+            // SignerInfo is defined as a collection in the RFC, but Apple Wallet should only have one
+            var signerInfo = SignerInfos.Single();
+
+            return Certificates.Single(c => c.CertInfo.SerialNumber == signerInfo.IssuerAndSerialNumber?.SerialNumber);
+        }
+    }
+
+    /// <summary>
+    /// Returns the intermediate certificate if it exists (some signatures don't include it).
+    /// </summary>
+    public Certificate? IntermediateCertificate
+    {
+        get
+        {
+            var issuerNames = Certificates.Select(c => c.CertInfo.Issuer.CommonName);
+
+            return Certificates.SingleOrDefault(c => issuerNames.Contains(c.CertInfo.Subject.CommonName));
+        }
+    }
+}
